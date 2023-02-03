136e6442957a0512b16740641412ae8af3330e5c
wmnlab <jackbedford0428@gmail.com>
2023-02-03 13:43:51 +0800

    update

--- experimental-tools-beta/gps/gps-data-get.sh
+++ experimental-tools-beta/gps/gps-data-get.sh
@@ -2,9 +2,16 @@
 
 # This script is to capture the location info from the GPS dongle
 
-#interval=1
-#FILE_PATH="sample"
-FILE_PATH=`(date +%Y_%m%d_%H%M)`"_GPS_info"
+path="log/`(date +%Y-%m-%d)`"
+    if [ ! -d $path ]
+        then
+            mkdir $path
+    fi
+
+# interval=1
+# FILE_PATH="sample"
+# FILE_PATH=`(date +%Y_%m%d_%H%M)`"_GPS_info"
+FILE_PATH="$path/`(date +%Y-%m-%d_%H-%M-%S)`_GPS_info"
 : > $FILE_PATH
 
 
