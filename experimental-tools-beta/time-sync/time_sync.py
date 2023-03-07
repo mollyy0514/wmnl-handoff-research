aeb906c8815d1a059cde623dd916e01746ac5787
chenyuanjye <jackbedford0428@gmail.com>
2023-03-08 02:07:13 +0800

    update

--- experimental-tools-beta/time-sync/time_sync.py
+++ experimental-tools-beta/time-sync/time_sync.py
@@ -18,20 +18,22 @@ if sys.argv[1] == '-c':
 
     i = 0
     while i <= num_packet_per_round:
-        outdata = str(i).zfill(3)
         time0 = time.time()
+        outdata = str(i).zfill(3)
         s.sendto(outdata.encode(), server_addr)
         s.settimeout(3)
         try:
             indata, addr = s.recvfrom(1024)
             time1 = time.time()
+            indata = indata.decode()
         except:
-            print("timeout",outdata[:4])
+            print("timeout", outdata[:3])
             continue
-        print(outdata[:4], time0, time1, "RTT =", (time1-time0)*1000, "ms")
+        print('recvfrom ' + str(addr) + ': ' + indata)
+        print(outdata[:3], time0, time1, "RTT =", (time1-time0)*1000, "ms")
         time_stamp.append([time0, time1])
         time.sleep(packet_interval)
-        i = i+1
+        i += 1
     outdata = "end"
     s.sendto(outdata.encode(), server_addr)
 
@@ -60,15 +62,11 @@ elif sys.argv[1] == '-s':
         if indata == 'end':
             break
         print('recvfrom ' + str(addr) + ': ' + indata[:3])
-        print(indata)
         
-        outdata = indata
         time1 = time.time()
+        outdata = f'{outdata[:3]} {time0} {time1}'
         s.sendto(outdata.encode(), addr)
-        print(outdata[:4], time0, time1)
-        csv_writer.writerow([outdata[:4], time0, time1])
-        
-        # outdata = f'{outdata[:4]}, {time0}, {time1}'
-        # s.sendto(outdata.encode(), addr)
+        print(outdata[:3], time0, time1)
+        csv_writer.writerow([outdata[:3], time0, time1])
 
     f.close()
