b87bb43f0e5675d96efe70a6aa121abea1272df7
chenyuanjye <jackbedford0428@gmail.com>
2023-08-24 17:54:03 +0800

    update

--- test.py
+++ test.py
@@ -1,60 +1,87 @@
-sorter = ['LTE_HO','MN_HO','SN_HO','MNSN_HO','SN_Rel','SN_Setup','SN_Rel_MN_HO','SN_Setup_MN_HO',
-          'SCG_Failure_t310-Expiry (0)','SCG_Failure_randomAccessProblem (1)','SCG_Failure_rlc-MaxNumRetx (2)','SCG_Failure_synchReconfigFailureSCG (3)',
-          'SCG_Failure_scg-ReconfigFailure (4)','SCG_Failure_srb3-IntegrityFailure (5)','SCG_Failure_other-r16 (6)',
-          'MCG_Failure_reconfigurationFailure (0)','MCG_Failure_handoverFailure (1)','MCG_Failure_otherFailure (2)',
-          'NAS_Recovery_reconfigurationFailure (0)','NAS_Recovery_handoverFailure (1)','NAS_Recovery_otherFailure (2)']
+df1 = dfs['nr'][0].copy()
+df2 = dfs['nr'][1].copy()
 
-D = {}
-for tag in sorter:
-    D[tag] = {'tables': [], 'intrs': [], 'count': []}
+df1 = df1[df1['serv_cel_index'] == 'PSCell'][['Timestamp', 'PCI', 'NR_ARFCN', 'RSRP', 'RSRQ']].reset_index(drop=True)
+df2 = df2[df2['serv_cel_index'] == 'PSCell'][['Timestamp', 'PCI', 'NR_ARFCN', 'RSRP', 'RSRQ']].reset_index(drop=True)
 
-for (exp, traces), devices, schemes in zip(exps.items(), _devices, _schemes):
-    for trace in traces:
-        for j, (dev, schm) in enumerate(zip(devices, schemes)):
-            ## read downlink
-            data = os.path.join(datadir, exp, dev, trace, 'data', 'udp_uplk_loss_latency.csv')
-            print(data, os.path.isfile(data))
-            df_ul = pd.read_csv(data)
-            df_ul = set_data(df_ul)
-            ## read handover
-            path = os.path.join(datadir, exp, dev, trace, 'data')
-            data = os.path.join(path, [s for s in os.listdir(path) if s.startswith('diag_log_') and s.endswith('_rrc.csv')][0])
-            print(data, os.path.isfile(data))
-            df_ho = pd.read_csv(data)
-            df_ho, _, _ = mi_parse_ho(df_ho, tz=8)
-            df_ho['ho_type0'] = df_ho['ho_type'].astype('string')
-            df_ho.loc[df_ho['cause'].notna(), 'ho_type0'] = df_ho['ho_type'].astype('string') + '_' + df_ho['cause'].astype('string')
-            df_ho['ho_type0'] = df_ho['ho_type0'].astype('category')
-            ## start processing
-            for tag in sorter:
-                # print('===================================')
-                # print(tag)
-                df = df_ho.query('ho_type0 == @tag').copy().reset_index(drop=True)
-                tables = []
-                intrs = []
-                if not df.empty:
-                    cname = ' '.join(df.at[0, 'ho_type'].split('_')) + ': ' + df.at[0, 'cause'] if pd.notna(df.at[0, 'cause']) else ' '.join(df.at[0, 'ho_type'].split('_'))
-                    for i, row in df.iterrows():
-                        start, end = df.at[i, 'start'], df.at[i, 'end']
-                        intr = df.at[i, 'intr'] if pd.notna(df.at[i, 'intr']) else 0
-                        table, intv = setup_profile(df_ul, start, end, mode='pyl', sec=10)
-                        # print(intr)
-                        # display(table)
-                        tables.append(table)
-                        intrs.append(intr)
-                    table, intr = merge_profile(tables, intrs)
-                    # print('Avg Duration:', intr, 'seconds')
-                    # display(table)
-                    # plot_profile(table, intr=intr, title=cname, mode='lost')
-                    # plot_profile(table, intr=intr, title=cname, mode='plr')
-                else:
-                    table = pd.DataFrame(columns=['cat_id','tx_count','lost','PLR'])
-                    table['cat_id'] = table['cat_id'].astype('float32')
-                    table['tx_count'] = table['tx_count'].astype('Int32')
-                    table['lost'] = table['lost'].astype('Int32')
-                    table['PLR'] = table['PLR'].astype('float32')
-                    intr = 0
-                # print('HO Count:', len(df))
-                D[tag]['tables'].append(table)
-                D[tag]['intrs'] = [*D[tag]['intrs'], *intrs]
-                D[tag]['count'].append(len(df))
+axes[10].plot([], [], color='tab:red', alpha=0.7, label='nr_rsrp_radio1')
+axes[10].plot([], [], color='tab:green', alpha=0.7, label='nr_rsrp_radio2')
+
+ax_twinx = axes[10].twinx()
+ax_twinx.plot(df1['Timestamp'], df1['RSRP'], color='tab:red', alpha=0.7, label='nr_rsrp_radio1')
+ax_twinx.plot(df2['Timestamp'], df2['RSRP'], color='tab:green', alpha=0.7, label='nr_rsrp_radio2')
+ax_twinx.set_ylim(-156, -31)
+ax_twinx.axhline(y=-110, color='gray', linestyle='--', linewidth=1.1)
+
+def drop_unchanged(df):
+    
+    df['prev_PCI'] = df['PCI'].shift(1)
+    df['prev_NR_ARFCN'] = df['NR_ARFCN'].shift(1)
+    df['handover'] = (df['PCI'] != df['prev_PCI']) | (df['NR_ARFCN'] != df['prev_NR_ARFCN'])
+    df.loc[0, 'handover'] = True
+    
+    df = df[df['handover']].reset_index(drop=True).drop(['prev_PCI', 'prev_NR_ARFCN'], axis=1)
+    df['duplicate'] = df.duplicated(subset=['PCI', 'NR_ARFCN'])
+    
+    pairs = [(pci, earfcn) for pci, earfcn in zip(df[~df['duplicate']]['PCI'].array, df[~df['duplicate']]['NR_ARFCN'].array)]
+    
+    return df, pairs
+
+def drop_duplicate(my_list):
+    unique_list = []
+    seen = set()
+    
+    for item in my_list:
+        if item not in seen:
+            unique_list.append(item)
+            seen.add(item)
+
+    return unique_list
+
+df1, pairs1 = drop_unchanged(df1)
+df2, pairs2 = drop_unchanged(df2)
+
+df = pd.concat([df1, df2], ignore_index=True).sort_values(by='Timestamp').reset_index(drop=True)
+pairs = [(pci, earfcn) for pci, earfcn in zip(df[~df['duplicate']]['PCI'].array, df[~df['duplicate']]['NR_ARFCN'].array)]
+
+cmap = plt.get_cmap('jet', len(pairs1))
+colors = [matplotlib.colors.to_hex(cmap(i)) for i in range(cmap.N)]
+cdict1 = {pair: color for pair, color in zip(pairs1, colors)}
+
+cmap = plt.get_cmap('gist_rainbow', len(pairs2))
+colors = [matplotlib.colors.to_hex(cmap(i)) for i in range(cmap.N)]
+cdict2 = {pair: color for pair, color in zip(pairs2, colors)}
+
+markers = ['p', '*', 'h', 'H', 'D', 'd', 'P', 'X']
+mdict = {}  
+
+axes[10].set_ylim(0, 1)
+axes[10].set_yticks([0.25, 0.75])
+axes[10].set_yticklabels(["Radio-1", "Radio-2"], rotation='vertical', va='center')
+
+k = 0       
+for pair in pairs1:
+    pci_earfcn = f'{pair[0]}:{pair[1]}'
+    
+    df = df1.copy()
+    tmp = df[(df['PCI'] == pair[0]) & (df['NR_ARFCN'] == pair[1])].reset_index(drop=True)
+    if not tmp.empty:
+        axes[10].vlines(tmp.Timestamp, ymin=0, ymax=0.55, color=cdict1[pair], linewidth=0.7, alpha=0.85, label=f'r1 {pci_earfcn}')
+    
+    tmp = tmp.drop(df.index[0])
+    if not tmp.empty:
+        axes[10].scatter(tmp.Timestamp, [0.05]*len(tmp), c=cdict1[pair], marker=markers[k % len(markers)], label=f'r1 {pci_earfcn} dup')
+        k += 1
+
+for pair in pairs2:
+    pci_earfcn = f'{pair[0]}:{pair[1]}'
+    
+    df = df2.copy()
+    tmp = df[(df['PCI'] == pair[0]) & (df['NR_ARFCN'] == pair[1])].reset_index(drop=True)
+    if not tmp.empty:
+        axes[10].vlines(tmp.Timestamp, ymin=0.45, ymax=1, color=cdict2[pair], linewidth=0.7, alpha=0.85, label=f'r2 {pci_earfcn}')
+    
+    tmp = tmp.drop(df.index[0])
+    if not tmp.empty:
+        axes[10].scatter(tmp.Timestamp, [0.95]*len(tmp), c=cdict2[pair], marker=markers[k % len(markers)], label=f'r2 {pci_earfcn} dup')
+        k += 1
