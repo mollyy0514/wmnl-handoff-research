2731a6e8c272def65b1400d46bbde5c653df5fe9
chenyuanjye <jackbedford0428@gmail.com>
2023-08-29 20:28:47 +0800

    update

--- experimental-tools-beta/tcp-socket-programming/v3/client.py
+++ experimental-tools-beta/tcp-socket-programming/v3/client.py
@@ -247,11 +247,11 @@ def transmit(sockets):
     print("transmit", seq, "packets")
 
 # Create and start DL receiving multi-thread
-rx_threads = []
-for s, dev in zip(rx_sockets, devices):
-    t_rx = threading.Thread(target=receive, args=(s, dev, ), daemon=True)
-    rx_threads.append(t_rx)
-    t_rx.start()
+# rx_threads = []
+# for s, dev in zip(rx_sockets, devices):
+#     t_rx = threading.Thread(target=receive, args=(s, dev, ), daemon=True)
+#     rx_threads.append(t_rx)
+#     t_rx.start()
 
 # Create and start UL transmission multi-processing
 # p_tx = multiprocessing.Process(target=transmit, args=(tx_sockets,), daemon=True)
--- experimental-tools-beta/tcp-socket-programming/v3/server.py
+++ experimental-tools-beta/tcp-socket-programming/v3/server.py
@@ -296,8 +296,8 @@ for conn, dev, port in zip(rx_connections, devices, ports):
 # p_tx = multiprocessing.Process(target=transmit, args=(tx_sockets,), daemon=True)
 # p_tx = multiprocessing.Process(target=transmit, args=(tx_connections,), daemon=True)
 
-t_tx = threading.Thread(target=transmit, args=(tx_connections,), daemon=True)
-t_tx.start()
+# t_tx = threading.Thread(target=transmit, args=(tx_connections,), daemon=True)
+# t_tx.start()
 
 
 # start = input('Start transmission? (y/n) ')
