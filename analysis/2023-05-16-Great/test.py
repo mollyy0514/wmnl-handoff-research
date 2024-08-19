303867b01790aaf16651551142617631727c81ae
shengru55688 <riven88926@gmail.com>
2024-08-19 08:18:32 +0800

    prune redundant files

--- analysis/2023-05-16-Great/test.py
+++
@@ -1,4 +0,0 @@
-st_seq = max([dfs[key][0]['seq'].array[0], dfs[key][1]['seq'].array[0]])
-    ed_seq = min([dfs[key][0]['seq'].array[-1], dfs[key][1]['seq'].array[-1]])
-    dfs[key][0] = dfs[key][0][(dfs[key][0]['seq'] >= st_seq) & (dfs[key][0]['seq'] <= ed_seq)].reset_index(drop=True)
-    dfs[key][1] = dfs[key][1][(dfs[key][1]['seq'] >= st_seq) & (dfs[key][1]['seq'] <= ed_seq)].reset_index(drop=True)
--- analysis/2023-06-16/test.py
+++
@@ -1,4 +0,0 @@
-df_ho, _, _ = mi_parse_ho(df_ho, tz=8)
-            df_ho['ho_type0'] = df_ho['ho_type'].astype('string')
-            df_ho.loc[df_ho['cause'].notna(), 'ho_type0'] = df_ho['ho_type'].astype('string') + '_' + df_ho['cause'].astype('string')
-            df_ho['ho_type0'] = df_ho['ho_type0'].astype('category')
